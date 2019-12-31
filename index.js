593352a3531d130c07780216f09dfdbccceb7512
Nuno Santos <nunojsantos@hotmail.com>
2019-12-31 09:38:36 +0000

    improve console output with some errors feedback

--- index.js
+++ index.js
@@ -2,5 +2,17 @@ const fizzbuzz = require('./fizzbuzz')
 
 const passedArguments = process.argv.slice(2)
 
+console.log('==>> The Amazing FizzBuzz <<==')
+
+if(passedArguments.length < 1) {
+  console.log(' ERROR:  a number as an argument is needed to use this amazing tool\n')
+  return
+}
+
+if(isNaN(passedArguments[0])) {
+  console.log(` ERROR:  '${passedArguments[0]}' is not a number... try it with 1, 2, 3 or 73584. You have plenty of choices\n`)
+  return
+}
+
 const result = fizzbuzz(passedArguments[0])
-console.log(`${result}\n`)
+console.log(` OUTPUT:  ${result}\n`)
