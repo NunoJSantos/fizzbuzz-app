f11f44e05f665b372f0a7e99f8ce293adb359f0a
Nuno Santos <nunojsantos@hotmail.com>
2019-12-31 09:05:49 +0000

    add all the fizzbuzz logic

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,31 @@
 const fizzbuzz = number => {
+  const FIZZ = 'fizz'
+  const BUZZ = 'buzz'
+  const LUCKY = 'lucky'
+
+  const containsTheNumber3 = number.toString().indexOf(3) > -1
+  const isMultipleOf3 = number % 3 === 0
+  const containsTheNumber5 = number.toString().indexOf(5) > -1
+  const isMultipleOf5 = number % 5 === 0
+
+  if(isMultipleOf3 && isMultipleOf5 && (containsTheNumber3 || containsTheNumber5))
+    return LUCKY
+
+  if(isMultipleOf3 && isMultipleOf5)
+    return `${FIZZ}${BUZZ}`
+
+  if(isMultipleOf3 && containsTheNumber3)
+    return `${FIZZ} ${FIZZ}`
+  
+  if(isMultipleOf3 || containsTheNumber3)
+    return FIZZ
+  
+  if(isMultipleOf5 && containsTheNumber5)
+    return `${BUZZ} ${BUZZ}`
+
+  if(isMultipleOf5 || containsTheNumber5)
+    return BUZZ
+
   return number
 }
 
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -4,3 +4,28 @@ test("returns the passed number if it's not a multiple nor contains the numbers 
   const result = fizzbuzz(1)
   expect(result).toBe(1)
 })
+
+test("returns 'lucky' if it's a multiple of 3, multiple of 5 and if contains 3 or 5 in its representation",  () => {
+  const result = fizzbuzz(15)
+  expect(result).toBe('lucky')
+})
+
+test("returns 'fizz fizz' if it's a multiple of 3 and contains 3 in its representation", () => {
+  const result = fizzbuzz(3)
+  expect(result).toBe('fizz fizz')
+})
+
+test("returns 'buzz buzz' if it's a multiple of 5 and contains 5 in its representation", () => {
+  const result = fizzbuzz(5)
+  expect(result).toBe('buzz buzz')
+})
+
+test("returns 'fizz' if it's a multiple of 3 or contains 3 in its representation, but not both", () => {
+  const result = fizzbuzz(6)
+  expect(result).toBe('fizz')
+})
+
+test("returns 'buzz' if it's a multiple of 5 or contains 5 in its representation, but not both", () => {
+  const result = fizzbuzz(10)
+  expect(result).toBe('buzz')
+})
